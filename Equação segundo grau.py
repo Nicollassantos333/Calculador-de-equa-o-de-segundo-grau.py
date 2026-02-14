06e57923a03217f97b1568ddec582c8062d3aa35
Nicollas Santos <nicollassantos660@gmail.com>
2026-02-14 15:25:54 -0300

    Adição de uma validação de números e correções de termos.

--- Equação segundo grau.py
+++ Equação segundo grau.py
@@ -1,7 +1,17 @@
 from math import sqrt
-a = int(input('Digite o número A da equação: '))
-b = int(input('Digite o número B da equação: '))
-c = int(input('Digite o número C da equação: '))
+
+def validador(tqsn):
+        while True:
+                     valor = input(tqsn)
+                     try: 
+                            return float(valor)
+                     except ValueError:
+                             print('Tente novamente digitando apenas números.')
+                     
+
+a = validador('Digite o número A da equação: ')
+b = validador('Digite o número B da equação: ')
+c = validador('Digite o número C da equação: ')
 
 #Delta 
 delta = float((b ** 2) - 4 * a * c)
@@ -12,5 +22,5 @@ if delta <0:
        print('Delta é negativo, a conta acaba por aqui')
 else:
         print((f'DELTA = {b}² - 4 x {a} x {c} é igual a: {delta}'))
-        print(f' X1 positivo = {b} + {sqrt(delta)} / 2 x {a} é igual a: {(-b + sqrt(delta)) / (2 * a)}')
-        print(f' X2 negativo = {b} - {sqrt(delta)} / 2 x {a} é igual a: {(-b - sqrt(delta)) / (2 * a)}')
+        print(f' X1 (adição) = {b} + {sqrt(delta)} / 2 x {a} é igual a: {(-b + sqrt(delta)) / (2 * a)}')
+        print(f' X2 (subtração) = {b} - {sqrt(delta)} / 2 x {a} é igual a: {(-b - sqrt(delta)) / (2 * a)}')
