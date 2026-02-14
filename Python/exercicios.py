d771d4266cdf52201260dad6a54a92279f08b737
Nicollas Santos <nicollassantos660@gmail.com>
2026-02-14 15:12:05 -0300

    arquivo deletado

--- Python/exercicios.py
+++
@@ -1,9 +0,0 @@
-def teste(numero):
-    while True:
-        try:
-            tqsn = float(input(numero))
-            return tqsn
-        except ValueError:
-            print('Tem que ser numero')
-
-a = teste('Digite um número: ')
--- Python/main.py
+++
@@ -1,6 +0,0 @@
-def cal(a, b):
-   print(a)
-   print(b)
-   print(a + b)
-   print('-'*30)
-cal(input('Insira os números: '))
